f09f0866cab4754b9fe793b601040b941accc2f0
Steven717900 <pospinachico@gmail.com>
2024-11-13 21:37:21 -0500

    Se crea el metodo de iniciar batalla
    :) bonitos y gorditos muchachos

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,5 +1,8 @@
 package simulador.batalla;
-
+import simulador.pokemon.Pokemon; 
+//Se importa para que
 public class Batalla {
     // TODO: Aquí va tu código
+    public void iniciarBatalla(Pokemon pokemon1,Pokemon pokemon2){
+    }
 }
